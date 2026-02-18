724d8450ebe09d6fb0ca984972be0d9851fc8f33
r0chd <100892812+unixpariah@users.noreply.github.com>
2026-02-18 22:14:15 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.tooling.url = "github:mox-desktop/tooling";
+
+  outputs =
+    { tooling, ... }:
+    tooling.lib.mkMoxFlake {
+      devShells = tooling.lib.forAllSystems (pkgs: {
+        default = pkgs.mkShell (
+          pkgs.lib.fix (finalAttrs: {
+            buildInputs = builtins.attrValues {
+              inherit (pkgs)
+                rustToolchain
+                rust-analyzer-unwrapped
+                nixd
+                ;
+            };
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath finalAttrs.buildInputs;
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          })
+        );
+      });
+    };
+}
